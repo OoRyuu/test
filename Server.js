d12e07ce61a156b8180dfcbaa5d77a5ef18d58c7
OoRyuu <Drago12342017@gmail.com>
2023-05-17 16:43:47 +0200

    test
    test

---
+++ Server.js
@@ -0,0 +1,24 @@
+const ws = require ( "ws" );
+const wss = new ws.Server( { port : 3000 } );
+
+all_users = { };
+// utente1 : conn,
+// utente2 : conn,
+// ...
+
+wss.on( "connection", conn => { 
+    console.log( "Nuovo Utente Conesso!" );
+
+
+    conn.on( "message", e => {
+        var msg_intero = JSON.parse( e );
+        if( msg_intero[ "destination" ] == "" && msg_intero[ "message" ] == "" )
+        {
+            all_users[ msg_intero[ "origin" ] ] = conn;
+        }else{
+            if ( all_users.hasOwnProperty( msg_intero["destination" ] ) ){
+                all_users[ msg_intero[ "destination" ] ].send( msg_intero[ "message" ] );
+            }
+        }
+    } );
+} );
